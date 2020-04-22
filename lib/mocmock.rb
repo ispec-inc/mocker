dc320fc024fe3d34a2600f972b26d9e5046249b0
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-04-22 23:14:37 +0900

    [fix] mocmock.rb

--- lib/mocmock.rb
+++ lib/mocmock.rb
@@ -1,10 +1,11 @@
 require_relative "./mocmock/version"
+require 'fileutils'
 
 module MocMock
   class Error < StandardError; end
 
   def self.init(dir)
-    p dir
-
+    FileUtils.mkdir_p(dir)
+    FileUtils.cp_r("inventory", dir)
   end
 end
