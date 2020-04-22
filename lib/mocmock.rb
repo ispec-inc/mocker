14bf93345223cbed95a84ea6a4c51a7f70e71cd7
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-04-22 23:49:04 +0900

    [create] mocmock init

--- lib/mocmock.rb
+++ lib/mocmock.rb
@@ -3,9 +3,16 @@ require 'fileutils'
 
 module MocMock
   class Error < StandardError; end
+  INVENTORY= "inventory"
 
   def self.init(dir)
     FileUtils.mkdir_p(dir)
-    FileUtils.cp_r("inventory", dir)
+
+    Dir.glob("#{INVENTORY}/**/*").each do |f|
+      nf = f.sub(INVENTORY, "")
+      File.ftype(f) == "file" ?
+        FileUtils.cp(f, "#{dir}/#{nf}") :
+        FileUtils.mkdir_p("#{dir}/#{nf}")
+    end
   end
 end
